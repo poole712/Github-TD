232d99bf99022228ee8d6669a0f9c6dafa71855a
Zach Stott <spywoawe@gmail.com>
2019-10-22 11:43:07 +1300

    CTRL Time Scale

--- New Unity Project/Assets/Scripts/Currency.cs
+++ New Unity Project/Assets/Scripts/Currency.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Currency : MonoBehaviour
 {
     public static int currency;
+    public Text curr;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("currencyTick");
+        curr.text = "Currency: " + 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        curr.text = "Currency: " + currency;
     }
 
     IEnumerator currencyTick()
