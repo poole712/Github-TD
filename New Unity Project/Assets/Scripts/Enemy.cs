025075b42742ddf52827e5b9f4e020df85ad51b7
Zach Stott <spywoawe@gmail.com>
2019-10-23 10:51:40 +1300

    Better enemy management

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int reward;
+
+    public float speed;
+
+    public Transform homeBase;
+
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = homeBase.position;
+
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health--;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/NormalEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NormalEnemy : Enemy
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = homeBase.position;
+
+        speed = Random.Range(2f, 6f);
+
+        agent.speed += speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+            if (health == 0)
+            {
+                Currency.currency = Currency.currency + reward;
+                Debug.Log(Currency.currency);
+                Destroy(this.gameObject);
+            }
+        
+    }
+}
